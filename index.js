256fa8d568f1fd8b1e9331ba0fbd9833ce9c36b7
shamil <for.photos.only001@gmail.com>
2026-05-21 23:45:58 +0530

    server update

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ const connectDB = require('./config/db')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.get('/',(req,res)=>{
+    res.status(200).json({
+        message:'welcome to pm-tool server'
+    })
+})
+
 const licenseRoute = require('./routes/licenseRoute')
 app.use("/",licenseRoute)
+
+app.get('/test',(req,res)=>{
+    res.status(200).json({
+        message:'test route success backend running successfully'
+    })
+})
 
 app.listen(PORT,async()=>{
     await connectDB()
